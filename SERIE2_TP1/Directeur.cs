ebfd501b5c9c80765b040b3ea844dad986f48037
OuaailElaouad <2003waelelaouad@gmail.com>
2025-02-26 22:34:32 +0000

    serie2 tp1

---
+++ SERIE2_TP1/Directeur.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERIE2_TP1
+{
+    internal class Directeur
+    {
+        private static Directeur instance;
+        private GestionEmployes gestionEmployes;
+
+        private Directeur()
+        {
+            gestionEmployes = new GestionEmployes();
+        }
+
+        public static Directeur GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Directeur();
+            }
+            return instance;
+        }
+
+        public void SetGestionEmployes(GestionEmployes gestion)
+        {
+            gestionEmployes = gestion;
+        }
+
+        public void AfficherStatistiques()
+        {
+            Console.WriteLine($"\n=== Statistiques de l'entreprise ===");
+            Console.WriteLine($"Salaire total : {gestionEmployes.CalculerSalaireTotal()} Dhs");
+            Console.WriteLine($"Salaire moyen : {gestionEmployes.CalculerSalaireMoyen()} Dhs");
+        }
+    }
+}
