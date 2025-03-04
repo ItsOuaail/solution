111b0acaf37a391605b424db80da8ed544d4dc21
OuaailElaouad <2003waelelaouad@gmail.com>
2025-03-04 23:41:01 +0000

    class personnel

---
+++ GestionRH/Models/Personnel.cs
@@ -0,0 +1,13 @@
+namespace GestionRH.Models
+{
+    public abstract class Personnel : Personne
+    {
+        public string Bureau { get; set; }
+        public Personnel(string code, string nom, string prenom, string bureau) : base(code, nom, prenom)
+        {
+            this.Bureau = bureau;
+        }
+
+        public abstract double CalculerSalaire();
+    }
+}
